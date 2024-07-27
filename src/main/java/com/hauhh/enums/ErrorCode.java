7ebeb03e0a929f350605143e3832d5c41fba0db2
Hoang Hai Hau <hhhau1910@gmail.com>
2024-07-27 17:46:48 +0700

    add new Enum in ErrorCode

--- src/main/java/com/hauhh/enums/ErrorCode.java
+++ src/main/java/com/hauhh/enums/ErrorCode.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-
-    ;
+    USER_NOT_FOUND(1001, "User not found"),
+    INVALID_INPUT(1002, "Invalid input");
 
     private final int code;
 
--- src/main/java/com/hauhh/exception/GlobalHandlerException.java
+++ src/main/java/com/hauhh/exception/GlobalHandlerException.java
@@ -15,14 +15,14 @@ public class GlobalHandlerException {
     private final static Logger logger = LoggerFactory.getLogger(GlobalHandlerException.class);
 
     @ExceptionHandler(ApplicationException.class)
-    public ResponseData<ResponseError> handleApplicationException(ApplicationException applicationException){
+    public ResponseData<ResponseError> handleApplicationException(ApplicationException applicationException) {
         ErrorCode errorCode = applicationException.getErrorCode();
         logger.error("Application Exception: [ErrorCode: {} Message: {}]", errorCode.getCode(), errorCode.getMessage());
         return new ResponseData<>(HttpStatus.BAD_REQUEST.value(),
                 "Error Message: " + errorCode.getMessage(),
                 ResponseError.builder()
-                .statusCode(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build());
+                        .statusCode(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
     }
 }
